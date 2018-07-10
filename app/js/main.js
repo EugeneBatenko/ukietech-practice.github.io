3b870227431479a07c93222f1905b233afc0873c
Yevhen Batenko <batenko.e.v@gmail.com>
2018-07-10 18:20:51 +0300

    Create mobile menu when resolution less 1024px

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
-//SEARCH FORM IN HEADER
-var search = document.getElementById("search-icon");
-function search_func(x) {
-    x.classList.toggle("change");
+//Animation of hamburger
+var mob_menu = document.getElementById("mobile-ul");
+function hamburger(x) {
+    x.classList.toggle("change-hamburger");
+    mob_menu.classList.toggle("mobile-ul-active");
 };
